6c707fe3354d52ac3f730aed3c3f81a3232ceea8
padma <tpadma.nin@gmail.com>
2023-04-24 09:37:37 -0400

    modified deploy.sh script to ahve only deployment

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Set execute permissions for the script
-chmod +x $0
-
-# Change ownership of the script to the current user
-chown $(whoami) $0
-
-# Stop and remove any existing containers
-docker stop resume-hosting || true && docker rm resume-hosting || true
-
-# Build the Docker image (Build)
-docker build -t resume-hosting .
-
 # Run the Docker container (Deploy)
 docker run -d -p 8081:80 --name resume-hosting resume-hosting
 
