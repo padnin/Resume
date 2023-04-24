3e5563f360ad91a86aff9cff062d90d9e9835648
padma <tpadma.nin@gmail.com>
2023-04-24 10:56:43 -0400

    port numbers revereted back to 8081

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM nginx:latest
 COPY index.html /usr/share/nginx/html/index.html
-EXPOSE 8082
+EXPOSE 8081
 CMD ["nginx", "-g", "daemon off;"]
--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ docker stop resume-hosting || true && docker rm resume-hosting || true
 docker build -t resume-hosting .
 
 # Run the Docker container (Deploy)
-docker run -d -p 8082:80 --name resume-hosting resume-hosting
+docker run -d -p 8081:80 --name resume-hosting resume-hosting
 
 # Check if the website is accessible (validation )
 
-if curl --output /dev/null --silent --head --fail http://localhost:8082; then
+if curl --output /dev/null --silent --head --fail http://localhost:8081; then
                                 echo "Website is accessible."
                             else
                                 echo "Website is not accessible. Exiting."
